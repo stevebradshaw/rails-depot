24b51d54e762cec72b0bee9b98a7a5a534d2915a
Steve Bradshaw <sjb1970@gmail.com>
2012-06-25 23:06:28 +0100

    Added stuff

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,11 +43,13 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product_id: product.id)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+#        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url }
+        format.js
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :quantity
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end
